8bcd7233247cd6de5c582364a815d9016740c623
hemanthgara <hemanthgara.hg@gmail.com>
2023-04-13 16:12:56 -0400

    User Error page added to the front end for error finding in the we application

---
+++ client/src/pages/ErrorPage/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div className='d-flex justify-content-center align-items-center fs-1 fw-bold' style={{width:"100vw", height: "100vh"}}>ErrorPage</div>
+  )
+}
+
+export default ErrorPage

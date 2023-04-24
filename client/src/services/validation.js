2afc3ffef975e92b76305cd44d8a9085643db3eb
hemanthgara <hemanthgara.hg@gmail.com>
2023-04-23 21:01:01 -0400

    Course Validations are added

--- client/src/services/validation.js
+++ client/src/services/validation.js
@@ -62,3 +62,69 @@ export const signinValidation = Yup.object({
     .required("Password is required")
     .min(8, "password must contain 8 or more characters"),
 });
+
+export const helpFormValidation = Yup.object({
+  name: Yup.string()
+    .min(3, "Must be 3 characters")
+    .max(15, "Must be 15 characters or less")
+    .required("Required"),
+
+  email: Yup.string().email("Invalid email address").required("Required"),
+
+  message: Yup.string().required("Required"),
+});
+
+export const addCourseValidation = Yup.object({
+  coursename: Yup.string().required("Required"),
+
+  coursedetail: Yup.string().required("Required"),
+
+  aboutcourse: Yup.string().required("Required"),
+
+  courselanguage: Yup.string().required("Required"),
+
+  coursefile: Yup.string().required("Required"),
+});
+
+export const courseContentValidation = Yup.object({
+
+  courseId: Yup.string().required("Required"),
+
+  title: Yup.string().required("Required"),
+
+  description: Yup.string().required("Required"),
+
+  image: Yup.string().required("Required"),
+
+  video: Yup.string().required("Required"),
+
+  pdf: Yup.string().required("Required"),
+});
+
+
+export const VideoDetailQueryValidation = Yup.object({
+  time: Yup.string().required("Required"),
+
+  title: Yup.string().required("Required"),
+
+  message: Yup.string().required("Required"),
+
+});
+
+export const QuestionValidation = Yup.object({
+  question: Yup.string().required("Required"),
+
+  option1: Yup.string().required("Required"),
+
+  option2: Yup.string().required("Required"),
+
+  option3: Yup.string().required("Required"),
+
+  option4: Yup.string().required("Required"),
+
+  correctOption: Yup.string().required("Required"),
+
+});
+
+
+

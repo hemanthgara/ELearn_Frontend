89c249c6d736e2ecdebf92cd38c85a31ef5f86e2
Srinivas_ganta <trupti.sagar97@gmail.com>
2023-04-28 21:25:05 -0400

    Home Page
    Page container in Home page File

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import "../../style/home.css"
+
+const Home = () => {
+  return (
+    <div className="home-page-container"><img src= "./elearning-portals-cover-picture.png" style={{ maxWidth: '100%'}}/></div>
+  )
+}
+
+export default Home

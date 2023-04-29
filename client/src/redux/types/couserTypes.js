6965544b133002695e5dd12cbb5ebac676476bdc
madhurimapothireddy <mpothireddy@albany.edu>
2023-04-29 14:20:48 -0400

    added coursetypes components

---
+++ client/src/redux/types/couserTypes.js
@@ -0,0 +1,10 @@
+export const SHOW_COURSE = "SHOW_COURSE";
+export const ADD_COURSE = "ADD_COURSE";
+export const FETCHING_COURSE = "FETCHING_COURSE";
+export const DELETE_COURSE = "DELETE_COURSE";
+
+
+export const SHOW_CONTENT = "SHOW_CONTENT";
+export const ADD_CONTENT = "ADD_CONTENT";
+export const FETCHING_CONTENT = "FETCHING_CONTENT";
+
